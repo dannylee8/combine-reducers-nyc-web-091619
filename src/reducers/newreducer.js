04f09c30bc70ddd4425257a477e343679e88e777
Danny Lee <dreamer@panix.com>
2020-01-16 16:31:02 -0500

    Done.

---
+++ src/reducers/newreducer.js
@@ -0,0 +1,34 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
+export default rootReducer
+
+function authorsReducer (state = [], action) {
+  let idx
+  switch (action.type) {
+    case 'ADD_AUTHOR':
+      return [...state, action.author]
+    case 'REMOVE_AUTHOR':
+      idx = state.findIndex(author => author.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    default:
+      return state
+  }
+}
+
+function booksReducer (state = [], action) {
+  let idx
+  switch (action.type) {
+    case 'ADD_BOOK':
+      return [...state, action.book]
+    case 'REMOVE_BOOK':
+      idx = state.findIndex(book => book.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+    default:
+      return state
+  }
+}
